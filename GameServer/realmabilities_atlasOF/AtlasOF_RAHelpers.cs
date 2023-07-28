45340bfe0488fe76737ff553f94c5ad652c6e12b
Gabriel Hollies <ghollies@gmail.com>
2023-07-28 09:07:06 -0400

    Shift 5 level RAs to 9
    * Update costs and values of RAs with 9 ranks

--- GameServer/realmabilities_atlasOF/AtlasOF_RAHelpers.cs
+++ GameServer/realmabilities_atlasOF/AtlasOF_RAHelpers.cs
@@ -47,12 +47,16 @@ namespace DOL.GS.RealmAbilities
 
             switch (level)
             {
-                case 1: return 5;
-                case 2: return 10;
-                case 3: return 15;
-                case 4: return 20;
-                case 5: return 25;
-                default: return 25;
+                case 1: return 2;
+                case 2: return 3;
+                case 3: return 5;
+                case 4: return 7;
+                case 5: return 10;
+                case 6: return 12;
+                case 7: return 15;
+                case 8: return 17;
+                case 9: return 20;
+                default: return 20;
             }
         }
 
@@ -62,13 +66,18 @@ namespace DOL.GS.RealmAbilities
         /// </summary>
         public static int GetCommonUpgradeCostFor5LevelsRA(int currentLevel)
         {
+
             switch (currentLevel)
             {
                 case 0: return 1;
-                case 1: return 3;
-                case 2: return 6;
-                case 3: return 10;
-                case 4: return 14;
+                case 1: return 1;
+                case 2: return 2;
+                case 3: return 3;
+                case 4: return 3;
+                case 5: return 5;
+                case 6: return 5;
+                case 7: return 7;
+                case 8: return 7;
                 default: return 1000;
             }
         }
