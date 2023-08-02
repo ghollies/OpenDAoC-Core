83cd18f8cfe8d4b6328ff303bf0ae14fd0e1da0c
Fenyn <midgetlord123@hotmail.com>
2023-08-02 15:16:08 -0700

    Ichor of the Deep rework

--- GameServer/realmabilities/handlers/IchorOfTheDeepAbility.cs
+++ GameServer/realmabilities/handlers/IchorOfTheDeepAbility.cs
@@ -11,12 +11,13 @@ namespace DOL.GS.RealmAbilities
 	public class IchorOfTheDeepAbility : TimedRealmAbility
 	{
 		public IchorOfTheDeepAbility(DBAbility dba, int level) : base(dba, level) { }
-
-		private ECSGameTimer m_expireTimerID;
-		private ECSGameTimer m_rootExpire;
-		private int dmgValue = 0;
-		private int duration = 0;
+		
+		private int dmgValue = 400;
+		private int duration = 30;
 		private GamePlayer caster;
+		private SpellLine m_spellline;
+		private Spell m_damageSpell;
+		private Spell m_rootSpell;
 
 		public override void Execute(GameLiving living)
 		{
@@ -37,7 +38,8 @@ namespace DOL.GS.RealmAbilities
 			var target = caster.TargetObject as GameLiving;
 
 			// So they can't use Admins or objects as a target
-			if (target == null || !GameServer.ServerRules.IsAllowedToAttack(caster, target, true))
+			if ((target == null || !GameServer.ServerRules.IsAllowedToAttack(caster, target, true)
+			    ) && caster.Client.Account.PrivLevel == 1)
 			{
 				caster.Out.SendMessage("You have an invalid target!", eChatType.CT_SpellResisted, eChatLoc.CL_SystemWindow);
 				caster.DisableSkill(this, 3 * 1000);
@@ -83,248 +85,59 @@ namespace DOL.GS.RealmAbilities
 				caster.DisableSkill(this, 3 * 1000);
 				return;
 			}
-
-			// Cannot use ability if timer is not expired
-			if (m_expireTimerID != null && m_expireTimerID.IsAlive)
-			{
-				caster.Out.SendMessage("You must wait" + m_expireTimerID.TimeUntilElapsed / 1000 + " seconds to recast this type of ability!", eChatType.CT_Spell, eChatLoc.CL_SystemWindow);
-				caster.DisableSkill(this, 3 * 1000);
-				return;
-			}
-
-			/*
-			if(ServerProperties.Properties.USE_NEW_ACTIVES_RAS_SCALING)
+			
+			if(m_damageSpell == null || m_spellline == null) CreateSpell();
+			
+			if(ServerProperties.Properties.USE_NEW_ACTIVES_RAS_SCALING && m_damageSpell != null)
 			{
 				switch (Level)
 				{
-						case 1: dmgValue = 150; duration = 10000; break;
-						case 2: dmgValue = 275; duration = 15000; break;
-						case 3: dmgValue = 400; duration = 20000; break;
-						case 4: dmgValue = 500; duration = 25000; break;
-						case 5: dmgValue = 600; duration = 30000; break;
-						default: return;
+					case 1: m_damageSpell.Damage = 150; m_damageSpell.Duration = 10000; break;
+					case 2: m_damageSpell.Damage = 275; m_damageSpell.Duration = 15000; break;
+					case 3: m_damageSpell.Damage = 400; m_damageSpell.Duration = 20000; break;
+					case 4: m_damageSpell.Damage = 500; m_damageSpell.Duration = 25000; break;
+					case 5: m_damageSpell.Damage = 600; m_damageSpell.Duration = 30000; break;
+					default: return;
 				}				
 			}
-				//150 dam/10 sec || 400/20  || 600/30
+			else
+			{
 				switch (Level)
 				{
-						case 1: dmgValue = 150; duration = 10000; break;
-						case 2: dmgValue = 400; duration = 20000; break;
-						case 3: dmgValue = 600; duration = 30000; break;
-						default: return;
+					case 1: dmgValue = 150; duration = 10000; break;
+					case 2: dmgValue = 400; duration = 20000; break;
+					case 3: dmgValue = 600; duration = 30000; break;
+					default: return;
 				}
-				*/
-
-			// Do the effect and damage if all went well... not sure why this is a timer
-			//m_expireTimerID = new ECSGameTimer(caster, new ECSGameTimer.ECSTimerCallback(EndCast), 1);
-			//m_expireTimerID.Start();
-			EndCast();
+			}
+			
+			caster.castingComponent.RequestStartCastSpell(m_damageSpell, m_spellline);
+			caster.DisableSkill(this, GetReUseDelay(Level));
 		}
-
-		protected virtual int EndCast()
+		
+		
+		public virtual void CreateSpell()
 		{
-			GameLiving living = caster.TargetObject as GameLiving;
-
-			foreach (GamePlayer i_player in caster.GetPlayersInRadius(WorldMgr.INFO_DISTANCE))
-			{
-				if (i_player == caster)
-					i_player.MessageToSelf("You cast " + this.Name + "!", eChatType.CT_Spell);
-				else
-					i_player.Out.SendMessage(caster.Name + " casts a spell!", eChatType.CT_Spell, eChatLoc.CL_SystemWindow);
-			}
-
-			/*if (living = caster && living.Realm != caster.Realm)
-			{
-				IchorEffect(living, living);
-			}
-			else
-			{
-				timer.Stop();
-				timer = null;
-				return 0;
-			}*/
-
-			// Hit all non-friendly mobs in radius, including the target
-			foreach (GameNPC mob in living.GetNPCsInRadius(500))
-			{
-				IchorEffect(living, mob);
-			}
-
-			// Do everything for GamePlayer now
-			foreach (GamePlayer aeplayer in living.GetPlayersInRadius(500))
-			{
-				IchorEffect(living, aeplayer);
-			}
-
-			DisableSkill(caster);
-			return 0;
-		}
-
-		private int CalculateDamageWithFalloff(int initialDamage, GameLiving initTarget, GameLiving aetarget)
-		{
-			//Console.WriteLine($"initial {initialDamage} caster {initTarget} target {aetarget}");
-			int modDamage = (int)Math.Round((decimal) (initialDamage * ((500-(initTarget.GetDistance(new Point2D(aetarget.X, aetarget.Y)))) / 500.0)));
-			//Console.WriteLine($"distance {((500-(initTarget.GetDistance(new Point2D(aetarget.X, aetarget.Y)))) / 500.0)} Mod {modDamage}");
-			return modDamage;
-		}
-
-		protected virtual int RootExpires(ECSGameTimer timer)
-		{
-			GameLiving living = timer.Owner as GameLiving;
-			if (living != null)
-			{
-				living.BuffBonusMultCategory1.Remove((int)eProperty.MaxSpeed, this);
-				SendUpdates(living);
-			}
-			timer.Stop();
-			timer = null;
-			return 0;
-		}
-		/// <summary>
-		/// Sends updates on effect start/stop
-		/// </summary>
-		/// <param name="owner"></param>
-		protected static void SendUpdates(GameLiving owner)
-		{
-			if (owner.IsMezzed || owner.IsStunned)
-				return;
-
-			GamePlayer player = owner as GamePlayer;
-			if (player != null)
-				player.Out.SendUpdateMaxSpeed();
-
-			GameNPC npc = owner as GameNPC;
-			if (npc != null)
-			{
-				short maxSpeed = npc.MaxSpeed;
-				if (npc.CurrentSpeed > maxSpeed)
-					npc.CurrentSpeed = maxSpeed;
-			}
-		}
-		/// <summary>
-		/// Handles attack on buff owner
-		/// </summary>
-		/// <param name="e"></param>
-		/// <param name="sender"></param>
-		/// <param name="arguments"></param>
-		protected virtual void OnAttacked(DOLEvent e, object sender, EventArgs arguments)
-		{
-			AttackedByEnemyEventArgs attackArgs = arguments as AttackedByEnemyEventArgs;
-			GameLiving living = sender as GameLiving;
-			if (attackArgs == null) return;
-			if (living == null) return;
-
-			switch (attackArgs.AttackData.AttackResult)
-			{
-				case eAttackResult.HitStyle:
-				case eAttackResult.HitUnstyled:
-					living.BuffBonusMultCategory1.Remove((int)eProperty.MaxSpeed, this);
-					SendUpdates(living);
-					break;
-			}
-		}
-
-		protected void IchorEffect(GameLiving centerTarget, GameLiving aoeTarget)
-		{
-			var living = centerTarget;
-			var target = aoeTarget;
-
-			if (living == null || target == null)
-				return;
-
-			dmgValue = 400;
-			duration = 20000;
-
-			#region Resists and Determination
-			var primaryResistModifier = target.GetResist(eDamageType.Spirit);
-			var secondaryResistModifier = target.SpecBuffBonusCategory[(int)eProperty.Resist_Spirit];
-			var rootdet = ((target.GetModified(eProperty.SpeedDecreaseDurationReduction) - 100) * -1);
-
-			var resistModifier = 0;
-			resistModifier += (int)((dmgValue * (double)primaryResistModifier) * -0.01);
-			resistModifier += (int)((dmgValue + (double)resistModifier) * (double)secondaryResistModifier * -0.01);
-
-			if (target is GamePlayer)
-				dmgValue += resistModifier;
-			else if (target is GameNPC)
-				dmgValue += resistModifier;
-
-			var rootmodifier = 0;
-			rootmodifier += (int)((duration * (double)primaryResistModifier) * -0.01);
-			rootmodifier += (int)((duration + (double)primaryResistModifier) * (double)secondaryResistModifier * -0.01);
-			rootmodifier += (int)((duration + (double)rootmodifier) * (double)rootdet * -0.01);
-
-			duration += rootmodifier;
-
-			if (duration < 1)
-				duration = 1;
-			#endregion Resists and Determination
-
-			// Ignore friendly players
-			if (target.Realm == caster.Realm || target == caster)
-				return;
-
-			if (!GameServer.ServerRules.IsAllowedToAttack(caster, target, true))
-				return;
-
-			//GameSpellEffect mez = SpellHandler.FindEffectOnTarget(aeplayer, "Mesmerize");
-			ECSGameEffect mez = EffectListService.GetEffectOnTarget(target, eEffect.Mez);
-			if (mez != null)
-				EffectService.RequestCancelEffect(mez);
-				//mez.Cancel(false);
-
-			// Falloff damage
-			int dmgWithFalloff = CalculateDamageWithFalloff(dmgValue, living, target);
-
-			target.TakeDamage(caster, eDamageType.Spirit, dmgWithFalloff, 0);
-			target.StartInterruptTimer(3000, AttackData.eAttackType.Spell, caster);
-
-			// Spell damage messages
-			caster.Out.SendMessage("You hit " + target.GetName(0, false) + " for " + dmgWithFalloff + " damage!", eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
-			// Display damage message to target if any damage is actually caused
-			if (dmgWithFalloff > 0 && target is GamePlayer gpTarget)
-				gpTarget.Out.SendMessage(caster.Name + " hits you for " + dmgWithFalloff + " damage!", eChatType.CT_Damaged, eChatLoc.CL_SystemWindow);
-
-			// Make sure they're not using SoS (needs fixing), Charge, or in Shade form
-			var targetCharge = EffectListService.GetEffectOnTarget(target, eEffect.Charge);
-			var targetShade = EffectListService.GetEffectOnTarget(target, eEffect.Shade);
-			var targetSoS = EffectListService.GetEffectOnTarget(target, eEffect.SpeedOfSound);
-			if (targetCharge == null && targetSoS == null && targetShade == null)
-			{
-				/*
-				// Send spell message to player if applicable
-				if (target is GamePlayer gpMessage)
-					gpMessage.Out.SendMessage("Constricting bonds surround your body!", eChatType.CT_Spell, eChatLoc.CL_SystemWindow);
-
-				// Apply the snare
-				target.BuffBonusMultCategory1.Set((int)eProperty.MaxSpeed, this, 1.0 - 99 * 0.01);
-				m_rootExpire = new ECSGameTimer(target, new ECSGameTimer.ECSTimerCallback(RootExpires), duration);
-				GameEventMgr.AddHandler(target, GameLivingEvent.AttackedByEnemy, new DOLEventHandler(OnAttacked));
-				SendUpdates(target);
-
-				// Send root animation and spell message
-				foreach (GamePlayer player in living.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-				{
-					player.Out.SendSpellEffectAnimation(caster, target, 7029, 0, false, 1);
-
-					if (player.IsWithinRadius(target, WorldMgr.INFO_DISTANCE) && player != target)
-						player.Out.SendMessage(target.GetName(0, false) + " is surrounded by constricting bonds!", eChatType.CT_Spell, eChatLoc.CL_SystemWindow);
-				}
-				*/
-				//Check if Ichor root is already on target. If it is, then reset duration.
-				var targetIchor = EffectListService.GetEffectOnTarget(target, eEffect.Ichor);
-				if(targetIchor != null)
-				{
-					//TODO - Refresh existing Ichor duration (or whatever the proper mechanic is?)
-				}
-				else
-					new AtlasOF_IchorECSEffect(new ECSGameEffectInitParams(target, duration, 1));
-			}
-			else
-				// Send resist animation if they cannot be rooted
-				foreach (GamePlayer player in living.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-					player.Out.SendSpellEffectAnimation(caster, target, 7029, 0, false, 0);
-
+			m_spellline = new SpellLine("RAs", "RealmAbilities", "RealmAbilities", true);
+			var damageSpell = new DBSpell();
+			damageSpell.Name = "Ichor of the Deep";
+			damageSpell.Icon = 7029;
+			damageSpell.ClientEffect = 7029;
+			damageSpell.Damage = dmgValue;
+			damageSpell.DamageType = 11;
+			damageSpell.Target = "enemy";
+			damageSpell.Radius = 500;
+			damageSpell.Type = eSpellType.DamageSpeedDecreaseNoVariance.ToString();
+			damageSpell.Value = 99;
+			damageSpell.Duration = duration;
+			damageSpell.Pulse = 0;
+			damageSpell.PulsePower = 0;
+			damageSpell.Power = 0;
+			damageSpell.CastTime = 2;
+			damageSpell.EffectGroup = 0;
+			damageSpell.Range = 1000;
+			damageSpell.Uninterruptible = true;
+			m_damageSpell = new Spell(damageSpell, 0); // make spell level 0 so it bypasses the spec level adjustment code
 		}
 
 		public override int GetReUseDelay(int level)
--- GameServer/realmabilities_atlasOF/handlers/AtlasOF_Ichor.cs
+++ GameServer/realmabilities_atlasOF/handlers/AtlasOF_Ichor.cs
@@ -8,8 +8,31 @@ namespace DOL.GS.RealmAbilities
     {
         public AtlasOF_Ichor(DBAbility dba, int level) : base(dba, level) { }
 
-        public override int MaxLevel { get { return 1; } }
-        public override int CostForUpgrade(int level, GamePlayer player) { return 14; }
+        public override int MaxLevel { get { return 5; } }
+        public override int CostForUpgrade(int level, GamePlayer player) { if (ServerProperties.Properties.USE_NEW_ACTIVES_RAS_SCALING)
+            {
+                switch (level)
+                {
+                    case 0: return 5;
+                    case 1: return 10;
+                    case 2: return 15;
+                    case 3: return 20;
+                    case 4: return 25;
+                    default: return 1000;
+                }
+            }
+            else
+            {
+                switch (level)
+                {
+                    case 0: return 1;
+                    case 1: return 3;
+                    case 2: return 6;
+                    case 3: return 10;
+                    case 4: return 14;
+                    default: return 1000;
+                }
+            } }
         public override int GetReUseDelay(int level) { return 900; } // 15 mins
     }
 }
